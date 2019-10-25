26ba94016ab36abd56cd29dda7b4ee4bff8662c4
Björn Grunde <bjorngrunde@live.se>
2019-10-25 20:56:01 +0200

    seperated api concerns, added private functions, cleaned tests

--- test/game_test.exs
+++ test/game_test.exs
@@ -13,26 +13,26 @@ defmodule GameTest do
   test "state isn't changed for :won or :lost game" do
     for state <- [:won, :lost] do
       game = Game.new_game() |> Map.put(:game_state, state)
-      assert {^game, _} = Game.make_move(game, "x")
+      assert {^game, _} = Hangman.make_move(game, "x")
     end
   end
 
   test "first occurence of letter is not already used" do
     game = Game.new_game()
-    {game, _tally} = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state != :already_used
   end
 
   test "second occurence of letter is already used" do
     game = Game.new_game()
-    {game, _tally} = Game.make_move(game, "x")
-    {game, _tally} = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
 
   test "a good guess is recognized" do
     game = Game.new_game("wibble")
-    {game, _tally} = Game.make_move(game, "w")
+    game = Game.make_move(game, "w")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
   end
@@ -75,7 +75,7 @@ defmodule GameTest do
       moves,
       Game.new_game(word),
       fn {guess, state, turns_left, word}, game ->
-        {game, _} = Game.make_move(game, guess)
+        {game, _} = Hangman.make_move(game, guess)
         assert game.game_state == state
         assert game.turns_left == turns_left
         game
