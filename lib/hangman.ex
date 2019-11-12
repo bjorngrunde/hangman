6209f235fcd0e0e02a0628e2ac33e8f1a3c2b809
Björn Grunde <bjorngrunde@live.se>
2019-11-12 13:39:31 +0100

    changes

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -1,6 +1,8 @@
 defmodule Hangman do
   def new_game() do
     {:ok, pid} = Supervisor.start_child(Hangman.Supervisor, [])
+    IO.puts("Started a new game server with process id: ")
+    IO.inspect(pid)
     pid
   end
 
